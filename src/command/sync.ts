9ee2f3a9b69f998e36b8c26bc540c74bc80d10db
rottenpen <rottenpen@outlook.com>
2021-11-02 09:20:17 +0800

    fix: 修复 pr 和 issue 混乱引起的bug

--- src/command/sync.ts
+++ src/command/sync.ts
@@ -14,30 +14,32 @@ export const sync = async (url: vscode.Uri) => {
     }
   );
   const issueList = await http.get(`/repos/${owner}/${repo}/issues`);
-  if (!issueList.data.length) {
+  const list = issueList.data.filter(
+    (ele: { pull_request: any }) => !ele.pull_request
+  );
+  if (!list.length) {
+    vscode.window.showErrorMessage("发生了一点错误～");
     return;
   }
-  let number = issueList.data[0].number;
+
+  let number = list[0].number;
   let code = fs.readFileSync(url.path).toString();
   // /repos/{owner}/{repo}/issues/{issue_number}/comments
-  const createComment = await http
-    .post(
-      `/repos/${owner}/${repo}/issues/${number}/comments`,
-      {
-        body: `\`\`\`javascript
+  const createComment = await http.post(
+    `/repos/${owner}/${repo}/issues/${number}/comments`,
+    {
+      body: `\`\`\`javascript
 ${code}
 \`\`\`
 * * *
 >来自 vscode 插件`,
+    },
+    {
+      headers: {
+        Authorization: `token ${account.accessToken}`,
       },
-      {
-        headers: {
-          Authorization: `token ${account.accessToken}`,
-        },
-      }
-    )
-    .catch((err) => {
-      console.log(err);
-    });
+    }
+  );
+
   return createComment;
 };
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,23 @@
 import * as vscode from "vscode";
 import { codeLensController } from "./codelen/CodeLensController";
 import { sync } from "./command/sync";
-
+let pending = false;
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "do-some-right.sync",
     async (rest) => {
-			await sync(rest);
-      vscode.window.showInformationMessage("你已成功 +1s!");
+      if (pending) {
+        vscode.window.showWarningMessage("正在同步...");
+        return;
+      }
+      pending = true;
+      try {
+        await sync(rest);
+        vscode.window.showInformationMessage("你已成功为公益事业 +1s!");
+      } catch (error) {
+        vscode.window.showErrorMessage(error as string);
+      }
+      pending = false;
     }
   );
 
