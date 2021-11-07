dca214e8142d3b8c64a04f760a72704d0a0655be
rottenpen <rottenpen@outlook.com>
2021-11-07 23:58:25 +0800

    feat: 获得每日一题题号

---
+++ src/command/getDaily.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import * as vscode from 'vscode';
+
+const http = axios.create({
+  baseURL: 'https://leetcode-cn.com/graphql/',
+  timeout: 10000,
+});
+/**
+ * 获得今天的每日一题题号
+ */
+export default async () => {
+  try {
+    let { data } = await http.post('', {
+      query:
+        '\n    query questionOfToday {\n  todayRecord {\n    date\n    userStatus\n    question {\n      questionId\n      frontendQuestionId: questionFrontendId\n      difficulty\n      title\n      titleCn: translatedTitle\n      titleSlug\n      paidOnly: isPaidOnly\n      freqBar\n      isFavor\n      acRate\n      status\n      solutionNum\n      hasVideoSolution\n      topicTags {\n        name\n        nameTranslated: translatedName\n        id\n      }\n      extra {\n        topCompanyTags {\n          imgUrl\n          slug\n          numSubscribed\n        }\n      }\n    }\n    lastSubmission {\n      id\n    }\n  }\n}\n    ',
+      variables: {},
+      operationName: 'questionOfToday',
+    });
+    vscode.window
+      .showInformationMessage(
+        `今天每日一题是：${data.data.todayRecord[0].question.frontendQuestionId} ${data.data.todayRecord[0].question.titleCn}`,
+        '打开题目吧',
+      )
+      .then(ele => {
+        vscode.commands.executeCommand('leetcode.searchProblem');
+      });
+  } catch (error) {
+    if ((error as any).message) {
+      vscode.window.showErrorMessage((error as any).message);
+    } else {
+      vscode.window.showErrorMessage(String(error));
+    }
+  }
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,25 @@
-import * as vscode from "vscode";
-import { codeLensController } from "./codelen/CodeLensController";
-import { sync } from "./command/sync";
+import * as vscode from 'vscode';
+import { codeLensController } from './codelen/CodeLensController';
+import { sync } from './command/sync';
+import getDaily from './command/getDaily';
 let pending = false;
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "do-some-right.sync",
-    async (rest) => {
-      if (pending) {
-        vscode.window.showWarningMessage("正在同步...");
-        return;
-      }
-      pending = true;
-      try {
-        await sync(rest);
-        vscode.window.showInformationMessage("你已成功为公益事业 +1s!");
-      } catch (error) {
-        vscode.window.showErrorMessage(error.message);
-      }
-      pending = false;
+  let disposable = vscode.commands.registerCommand('do-something-right.sync', async rest => {
+    if (pending) {
+      vscode.window.showWarningMessage('正在同步...');
+      return;
     }
-  );
-
-  context.subscriptions.push(disposable, codeLensController);
+    pending = true;
+    try {
+      await sync(rest);
+      vscode.window.showInformationMessage('你已成功为公益事业 +1s!');
+    } catch (error) {
+      vscode.window.showErrorMessage((error as any).message);
+    }
+    pending = false;
+  });
+  let dailyCommand = vscode.commands.registerCommand('do-something-right.getDaily', getDaily);
+  context.subscriptions.push(disposable, codeLensController, dailyCommand);
 }
 
 export function deactivate() {}
